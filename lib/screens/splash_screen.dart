931e2ef1df05ec4ea3a41c0c869a5e191f3aeeaa
promesses-alfa <info@promesses.it>
2025-06-09 15:04:54 +0200

    Fix splash screen crash with StatefulWidget

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'onboarding_screen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
     Future.delayed(const Duration(seconds: 2), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const OnboardingScreen()),
       );
     });
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text(
